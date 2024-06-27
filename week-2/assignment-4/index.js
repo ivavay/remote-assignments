30cf8b20e8054dd94f7b424cabdbaca5ba93cba2
Ivy Chen <ichen@ucsc.edu>
2024-06-27 20:17:42 +0800

    request 2 done

--- week-2/assignment-4/index.js
+++ week-2/assignment-4/index.js
@@ -1,5 +1,47 @@
+// Request 1: Click to change text 
+// Change the 'Welcome Message' header to 'Have a good time!' upon clicking 
+
 let headerBox = document.querySelector('header > h1')
 
 headerBox.addEventListener('click', function() {
     headerBox.innerHTML = 'Have a Good Time!'
 })
+
+// Request 2: Click to show/hide menu 
+
+// TODO Open the menu display upon clicking 
+
+let menu = document.querySelector('.hamburger-menu')
+let openMenu = document.querySelector('.opened-menu')
+let exitMenu = document.querySelector('.exit-menu')
+let minMenu = document.querySelector('.nav-item')
+
+function toggleMenu() {
+    if (openMenu.style.display == 'block') {
+        // console.log('x clicked')
+        menu.style.display = 'flex'
+        openMenu.style.display = 'none'
+        exitMenu.style.display = 'none'
+
+    } else {
+        // console.log("toggle")
+        menu.style.display = 'none'
+        openMenu.style.display = 'block'
+        exitMenu.style.display = 'block'
+    }
+   
+}
+
+menu.addEventListener('click', function() {
+    toggleMenu()
+})  
+exitMenu.addEventListener('click', function() {
+    toggleMenu()
+})
+
+// TODO Close the menu display upon clicking 'x' button 
+
+
+// Request 3: Click to show more content boxes 
+
+// TODO Expand to show more content boxes upon clicking 'CTA'
