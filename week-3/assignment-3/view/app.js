c68b85c2043800f02b70392f8de428206ef2fea8
Ivy Chen <ichen@ucsc.edu>
2024-07-05 10:35:36 +0800

    sum value gets displayed now but errors msgs don't work yet

--- week-3/assignment-3/view/app.js
+++ week-3/assignment-3/view/app.js
@@ -1,37 +1,53 @@
 const express = require('express')
 const app = express()
+// Body parser to parse form data 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 // Parse requests with JSON payloads
 app.use(express.json())
 // Define the server code 
 const port = 3000
 // Set default engine 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html')
+// app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs')
 app.set('views', __dirname);
 
 // Serving static files in experss
 app.use(express.static('view'))
 
 
-app.get('/sum.html', (req, res) => {
+app.get('/sum', (req, res) => {
+  res.render('sum')
+  } 
+)
+
+app.post('/sum', (req, res) => {
+  // Returns a JSON like { number: '4' } 
+  // res.send(req.body)
   // Try and catch works now
   try {
     // Makes sure the number is an integer
-    let number = parseInt(req.body);
+    const number = req.body.inputNum;
+    
     // If query is a positive number
-    if (Number.isInteger(number) && number > 0) {
+    if (number > 0) {
       const arrayItems = []
       // Loop through and add each index as a item in the array 
       for (let i = 1; i <= number; i++) {
           arrayItems.push(i)
       }
+      
       // Add the items together 
       let calculation = arrayItems.reduce((sum, value) => sum + value, 0)
   
       // Sends the queried number as the '1+2+3+4+5' as a response 
-      res.json(calculation)
-      
-    } else if (Object.keys(req.body).length === 0) {
+      // Convert calculation value to a string
+      // res.json(' + calculation')
+      console.log(calculation)
+      res.render('sum', {'message': calculation})
+      // res.render('sum', {'sum': calculation})
+
+    } else if (Object.keys(req.body.number).length === 0) {
       throw new Error ('Lack of Parameter')
     
       // If number parameter isn't an integer, then...
@@ -42,12 +58,6 @@ app.get('/sum.html', (req, res) => {
  catch (error) {
     res.send(error.message)
     }
-  } 
-)
-
-app.post('/sum.html', (req, res) => {
-  // Returns a JSON like { number: '4' } 
-  res.json(req.body)
 })
 
 
--- week-3/assignment-3/view/index.js
+++ week-3/assignment-3/view/index.js
@@ -1,28 +1,28 @@
-const form = document.querySelector('form')
-form.addEventListener('submit', postData)
+// const form = document.querySelector('form')
+// form.addEventListener('submit', postData)
 
-// Using the fetch API 
-function postData(e) {
-  const inputValue = document.getElementById("number").value
+// // Using the fetch API 
+// function postData(e) {
+//   const inputValue = document.getElementById("number").value
 
-  e.preventDefault()
-  console.log(inputValue)
+//   e.preventDefault()
+//   console.log(inputValue)
 
-  const config = {
-    method: 'POST', 
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    // Converts JS objects into strings
-    body: JSON.stringify({ number: inputValue })
-  }
+//   const config = {
+//     method: 'POST', 
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     // Converts JS objects into strings
+//     body: JSON.stringify({ number: inputValue })
+//   }
 
-  fetch('/sum.html', config)
-    .then(res => res.json())
-    .then(data =>{ 
-      console.log(data)
-      // Right now this takes the submitted number and display it as the message
-      document.getElementById("message").innerHTML = data.number
-    })
-}
+//   fetch('/sum.html', config)
+//     .then(res => res.json())
+//     .then(data =>{ 
+//       console.log(data)
+//       // Right now this takes the submitted number and display it as the message
+//       document.getElementById("message").innerHTML = data.number
+//     })
+// }
 
