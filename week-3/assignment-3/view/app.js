de017e5378f6f21d5f2757f1a5e0add00168ae3a
Ivy Chen <ichen@ucsc.edu>
2024-07-08 09:11:03 +0800

    fixed formating and added getData

--- week-3/assignment-3/view/app.js
+++ week-3/assignment-3/view/app.js
@@ -1,72 +1,97 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-// Body parser to parse form data 
-const bodyParser = require('body-parser');
+// Body parser to parse form data
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Parse requests with JSON payloads
-app.use(express.json())
+app.use(express.json());
 app.use(bodyParser.json());
 
-// Define the server code 
-const port = 3000
+// Define the server code
+const port = 3000;
 
-// Set default engine 
-app.set('view engine', 'ejs')
-app.set('views', __dirname);
+// Set default engine
+app.set("view engine", "ejs");
+app.set("views", __dirname);
 
 // Serving static files in express
-app.use(express.static('view'))
+app.use(express.static("view"));
 
-// Gets the sum page 
-app.get('/sum', (req, res) => {
-  res.render('sum')
-  } 
-)
+// GET the query and sends the result from logic back
+app.get("/getData", (req, res) => {
+  // Try and catch works now
+  try {
+    // Makes sure the number is an integer
+    let number = parseInt(req.query.number);
+    // If query is a positive number
+    if (Number.isInteger(number) && number > 0) {
+      const arrayItems = [];
+      // Loop through and add each index as a item in the array
+      for (let i = 1; i <= number; i++) {
+        arrayItems.push(i);
+      }
+      // Add the items together
+      let calculation = arrayItems.reduce((sum, value) => sum + value, 0);
+      // calculation = eval(calculation)
+      // Sends the queried number as the '1+2+3+4+5' as a response
+      res.send("Sum: " + calculation);
+      // If no parameter, then...
+      // Checks if the req.query is empty
+    } else if (Object.keys(req.query).length === 0) {
+      throw new Error("Lack of Parameter");
+      // If number parameter isn't an integer, then...
+    } else {
+      throw new Error("Wrong Paramter!!");
+    }
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
-// Renders the form and processes it 
-app.post('/sum', (req, res) => {
+// Gets the sum page
+app.get("/sum", (req, res) => {
+  res.render("sum");
+});
+
+// Renders the form and processes it
+app.post("/sum", (req, res) => {
   // Makes sure the number is an integer
   const number = parseInt(req.body.number);
   try {
     // If query is a positive number
     if (Number.isInteger(number) && number > 0) {
-      const arrayItems = []
-      // Loop through and add each index as a item in the array 
+      const arrayItems = [];
+      // Loop through and add each index as a item in the array
       for (let i = 1; i <= number; i++) {
-          arrayItems.push(i)
+        arrayItems.push(i);
       }
-      
-      // Add the items together 
-      let calculation = arrayItems.reduce((sum, value) => sum + value, 0)
-  
+
+      // Add the items together
+      let calculation = arrayItems.reduce((sum, value) => sum + value, 0);
+
       // console.log(calculation)
 
       // Renders the calculated sum as the message
-      res.json({ sum: calculation })
+      res.json({ sum: calculation });
 
-      // If there is no parameter, meaning the form submits nothing 
+      // If there is no parameter, meaning the form submits nothing
     } else if (Object.keys(req.body.number).length === 0) {
-      console.log('No parameter')
-      throw new Error ('Lack of Parameter!!')
-    
+      console.log("No parameter");
+      throw new Error("Lack of Parameter!!");
+
       // If number parameter isn't an integer, then...
     } else {
-      throw new Error('Wrong Parameter!!')
+      throw new Error("Wrong Parameter!!");
     }
-  }
- catch (error) {
+  } catch (error) {
     // res.render('sum', {message: error })
-    console.log('HEYLO')
-    res.json({ message : error.message })
-    }
-})
-
+    console.log("HEYLO");
+    res.json({ message: error.message });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
--- week-3/assignment-3/view/index.js
+++ week-3/assignment-3/view/index.js
@@ -1,38 +1,36 @@
-// Getting the form element 
-const form = document.querySelector('form')
-form.addEventListener('submit', postData)
+// Getting the form element
+const form = document.querySelector("form");
+form.addEventListener("submit", postData);
 
-// Fetch API 
+// Fetch API
 function postData(e) {
-    // console.log('HI')
-    e.preventDefault(e)
-    const number = document.getElementById("number").value
-    // console.log(number)
-    const config = {
-        method: 'POST', 
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        // Convert to JSON format 
-        body: JSON.stringify({number: number})
-    }
+  // console.log('HI')
+  e.preventDefault(e);
+  const number = document.getElementById("number").value;
+  // console.log(number)
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Convert to JSON format
+    body: JSON.stringify({ number: number }),
+  };
 
-    const url = '/sum'
+  const url = "/sum";
 
-    fetch(url, config)
-        .then(response => response.json())
-        .then(data => {
-            // If there is a error message from data, throw that custom err
-            if (data.message) {
-                console.error('custom error please')
-                throw new Error(data.message); 
-            }
-            document.getElementById("message").textContent = data.sum  
-            
-        })
-        .catch(error => {
-            console.log(error)
-            document.getElementById("message").textContent = error.message
-        });
-
+  fetch(url, config)
+    .then((response) => response.json())
+    .then((data) => {
+      // If there is a error message from data, throw that custom err
+      if (data.message) {
+        console.error("custom error please");
+        throw new Error(data.message);
+      }
+      document.getElementById("message").textContent = data.sum;
+    })
+    .catch((error) => {
+      console.log(error);
+      document.getElementById("message").textContent = error.message;
+    });
 }
