ba01c1fa0f5ec7b59c43feff3a1511247da1f214
Ivy Chen <ichen@ucsc.edu>
2024-07-08 09:16:56 +0800

    fixed formating

--- week-3/assignment-1/site/app.js
+++ week-3/assignment-1/site/app.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 // Parse requests with JSON payloads
-app.use(express.json())
-// Define the server code 
-const port = 3000
+app.use(express.json());
+// Define the server code
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, My Server')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, My Server");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
--- week-3/assignment-2/site/app.js
+++ week-3/assignment-2/site/app.js
@@ -1,47 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 // Parse requests with JSON payloads
-app.use(express.json())
-// Define the server code 
-const port = 3000
+app.use(express.json());
+// Define the server code
+const port = 3000;
 
-app.get('/getData', (req, res) => {
+app.get("/getData", (req, res) => {
   // Try and catch works now
   try {
     // Makes sure the number is an integer
     let number = parseInt(req.query.number);
     // If query is a positive number
-    if(Number.isInteger(number) && number > 0) {
-      const arrayItems = []
-      // Loop through and add each index as a item in the array 
+    if (Number.isInteger(number) && number > 0) {
+      const arrayItems = [];
+      // Loop through and add each index as a item in the array
       for (let i = 1; i <= number; i++) {
-          arrayItems.push(i)
+        arrayItems.push(i);
       }
-      // Add the items together 
-      let calculation = arrayItems.reduce((sum, value) => sum + value, 0)
+      // Add the items together
+      let calculation = arrayItems.reduce((sum, value) => sum + value, 0);
       // calculation = eval(calculation)
-      // Sends the queried number as the '1+2+3+4+5' as a response 
-      res.send("Sum: " + calculation)
+      // Sends the queried number as the '1+2+3+4+5' as a response
+      res.send("Sum: " + calculation);
       // If no parameter, then...
       // Checks if the req.query is empty
     } else if (Object.keys(req.query).length === 0) {
-      throw new Error ('Lack of Parameter')
+      throw new Error("Lack of Parameter");
       // If number parameter isn't an integer, then...
     } else {
-      throw new Error('Wrong Paramter!!')
+      throw new Error("Wrong Paramter!!");
     }
-    
+  } catch (error) {
+    res.send(error.message);
   }
-
- catch (error) {
-    res.send(error.message)
-    }
-  } 
-)
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
