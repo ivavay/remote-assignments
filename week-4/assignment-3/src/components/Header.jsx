7daa530bd7eb0940515ce42081654fa1ce032f52
Ivy Chen <ichen@ucsc.edu>
2024-07-08 16:49:46 +0800

    toggle and display message done

--- week-4/assignment-3/src/components/Header.jsx
+++ week-4/assignment-3/src/components/Header.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 export default function Header() {
+  // Chnage text upon clicking
+  const [text, setText] = useState("Welcome Message");
+  function handleClick() {
+    setText("Have a Good Day");
+  }
   return (
     <>
       <header>
-        <h1>Welcome Message</h1>
+        <h1 onClick={handleClick}>{text}</h1>
       </header>
     </>
   );
--- week-4/assignment-3/src/components/Nav.jsx
+++ week-4/assignment-3/src/components/Nav.jsx
@@ -1,19 +1,47 @@
+import { useState } from "react"
+import { useRef } from 'react';
+import exit from '../assets/exit.png'
 export default function Nav() {
+    const openMenu = useRef(null);
+    const exitMenu = useRef(null);
+    const menu = useRef(null);
+
+    const [toggle, setToggle] = useState(true)
+    function toggleMenu() {
+        console.log(toggle)
+        // openMenu.current.style.display = 'block'
+
+        if (toggle) {
+            setToggle(!toggle)
+            console.log("menu is open") 
+            console.log(toggle)
+            menu.current.style.display = 'none'
+            openMenu.current.style.display = 'block'
+            exitMenu.current.style.display = 'block'
+        } else {
+            setToggle(!toggle)
+            console.log("menu is not open")
+            console.log(toggle)
+            menu.current.style.display = 'flex'
+            openMenu.current.style.display = 'none'
+            exitMenu.current.style.display = 'none'
+        }
+    }
     return(
-        <div>
+        <nav>
             <ul>
             <li className="logo">Website Title / Logo</li>
             <li className="nav-item">Item 1</li>
             <li className="nav-item">Item 2</li>
             <li className="nav-item">Item 3</li>
             <li className="nav-item">Item 4</li>
-            <div className="hamburger-menu">
+            <div onClick={toggleMenu} ref={menu} className="hamburger-menu">
                 <div className="line"></div>
                 <div className="line"></div>
                 <div className="line"></div>
             </div>
-            <div className="opened-menu">
-                <div className="exit-menu"><img className="exit" src="images/exit.png" alt="exit" /></div>
+            <div ref={openMenu} className="opened-menu">
+                <div ref={exitMenu} onClick={toggleMenu} className="exit-menu"><img className="exit" src={exit} alt="exit" /></div>
                 <ul className="min-menu">
                     <li className="li-items">Item 1</li>
                     <li className="li-items">Item 2</li>
@@ -22,6 +50,6 @@ export default function Nav() {
                 </ul>
             </div>
         </ul>
-        </div>
+        </nav>
     )
 }
